54d451ce47f020e3882c556f07575fbe2515b269
Mauro Camara Escudero <maurocamaraescudero@gmail.com>
2020-03-20 16:13:29 +0000

    modified log gaussian ep with the genealogies likelihood

--- log_gaussian_ep.R
+++ log_gaussian_ep.R
@@ -1,6 +1,18 @@
 library(emdbook)   # For dmvnorm
-library(MASS)      # For rmvnorm
+library(MASS)      # For mvrnorm
 library(parallel)  # For mclapply
+# library(lgarch)    # For rmnorm
+library(mnormt)    # For dmnorm, rmnorm
+
+# Checks for positive definiteness
+is_positive_definite <- function(Sigma, tol=1e-8){
+  # Compute eigenvalues
+  eigenvalues <- eigen(Sigma, only.values = TRUE)$values
+  # If smaller then threshold, set to zero
+  eigenvalues[abs(eigenvalues) < tol] <- 0.0
+  # If any is <= 0 then it's not positive definite
+  return(ifelse(any(eigenvalues <= 0), FALSE, TRUE))
+}
 
 ### Random-Walk Metropolis-Hastings (on log scale)
 rwmh <- function(start, niter, logtarget, Sigma=NULL){
@@ -52,18 +64,24 @@ natural2moment <- function(r, Q){
 }
 
 ### Sample Moments from Tilted Distribution
-sample_moments_tilted <- function(site_index, local_likelihood, r_c, Q_c, nsamples){
+sample_moments_tilted <- function(site_index, local_likelihood, mu_c, Sigma_c, nsamples){
   # Create wrapper for tilted distribution. Will be used to sample from it
   # TODO: Check dimensions of input x and output. I use as.double() cause its a vector
-  log_target <- function(x){
-    log(local_likelihood(x)) + as.double(-0.5 * x %*% (Q_c %*% x) + t(r_c) %*% x) 
+  # Find moment parameters for cavity distribution (will be used by likelihood)
+  log_tilted <- function(theta){
+    # Evaluate log Cavity Distribution (prior) at theta using r_c and Q_c
+    # log_cavity <- as.double(-0.5 * theta %*% (Q_c %*% theta) + t(r_c) %*% theta)
+    log_cavity <- dmvnorm(theta, c(mu_c), Sigma_c, log=TRUE)
+    # Evaluate log Local Likelihood at the data contained at site `site_index`, given r_c and Q_c
+    log_likelihood <- local_likelihood(site_index, sigma=moment_cavity$Sigma, mu=c(moment_cavity$mu))
+    # Sum them log_cavity and log_likelihood to obtain log_tilted
+    # Compute Cavity Distribution Estimate
+    return(log_likelihood + log_cavity)
   }
-  # Grab mu of the cavity distribution as a starting point
-  mu_c <- solve(Q_c, r_c)
   # Sample from the targe distribution. First, find the mode and start sampling from there
-  sol <- optim(c(mu_c), function(x) -log_target(x), method="BFGS", hessian=TRUE)
+  sol <- optim(c(moment_cavity$mu), function(x) -log_tilted(x), method="BFGS", hessian=TRUE)
   inv_hessian <- chol2inv(chol(sol$hessian))
-  samples <- rwmh(start=sol$par, niter=nsamples, logtarget=log_target, Sigma=inv_hessian)
+  samples <- rwmh(start=sol$par, niter=nsamples, logtarget=log_tilted, Sigma=inv_hessian)
   # Compute mean and covariance of the samples, assign them to the new global moment parameters
   mu_global_new    <- matrix(apply(samples, 2, mean))
   Sigma_global_new <- cov(samples)
@@ -77,19 +95,29 @@ site_update <- function(ep, site_index, pass_index, nsamples, local_likelihood){
   # Compute natural parameters of cavity distribution
   Q_c <- ep$Q - ep$Q_list[,,site_index]
   r_c <- ep$r - matrix(ep$r_list[,,site_index])
-  # By sampling tilted, find new global natural parameters
-  rq_new <- sample_moments_tilted(site_index, local_likelihood, r_c, Q_c, nsamples)
-  # Find new local natural parameters by subtracting natural params of cavity distribution
-  r_i_new <- rq_new$r - ep$r + matrix(ep$r_list[,,site_index])
-  q_i_new <- rq_new$Q - ep$Q + ep$Q_list[,,site_index]
+  # Compute moment parameters to see whether to skip this site update
+  moment_cavity <- natural2moment(r_c, Q_c)
+  if (is_positive_definite(moment_cavity$Sigma)){
+    # By sampling tilted, find new global natural parameters
+    rq_new <- sample_moments_tilted(site_index, local_likelihood, moment_cavity$mu, moment_cavity$Sigma, nsamples)
+    # Find new local natural parameters by subtracting natural params of cavity distribution
+    r_i_new <- rq_new$r - ep$r + matrix(ep$r_list[,,site_index])
+    q_i_new <- rq_new$Q - ep$Q + ep$Q_list[,,site_index]
     # Update EP local parameters
     # Return the new local natural parameter so we can do updates in a parallel fashion
     return(list(r_i=r_i_new, 
                 q_i=q_i_new))
+  } else {
+    return(list(r_i=matrix(0, nrow=nparams, ncol=1),
+                q_i=matrix(0, nrow=nparams, ncol=nparams)))
+  }
 }
 
 ### Main function for EP Algorithm
 ep_algorithm <- function(nsites, nparams, nsamples, npasses, mu_prior, Sigma_prior, local_likelihood, alpha=1.0){
+  # Create arrays to store the history of the r and Q
+  history_r <- array(0, dim=c(nparams, 1, npasses))
+  history_Q <- array(0, dim=c(nparams, nparams, npasses))
   # Store prior natural parameters, arrays of Q_i and r_i, global Q and r
   natural_prior <- moment2natural(mu_prior, Sigma_prior)
   ep <- list(
@@ -107,7 +135,7 @@ ep_algorithm <- function(nsites, nparams, nsamples, npasses, mu_prior, Sigma_pri
     }
     # Find (mu, Sigma) of cavity distribution, construct tilted, sample from it 
     # and convert moment parameters to natural parameters.
-    #updates <- mclapply(1:nsites, site_update_wrapper, mc.cores = 4)
+    # updates <- mclapply(1:nsites, site_update_wrapper, mc.cores = 4)
     updates <- list()
     for (site in 1:nsites){
       updates[[site]] <- site_update_wrapper(site)
@@ -124,32 +152,73 @@ ep_algorithm <- function(nsites, nparams, nsamples, npasses, mu_prior, Sigma_pri
     # Update the global parameters with some damping 
     ep$r <- alpha*r_new + (1 - alpha)*(ep$r)
     ep$Q <- alpha*Q_new + (1 - alpha)*(ep$Q)
+    # Add global mu and global Sigma to history to check convergence
+    history_r[,,pass_index] <- ep$r
+    history_Q[,,pass_index] <- ep$Q
   }
-  # TODO: Should I move this inside the main loop for printing reasons?
-  global_moments <- natural2moment(ep$r, ep$Q)
-  ep$mu    <- global_moments$mu
-  ep$Sigma <- global_moments$Sigma
-  return(ep)
+  return(list(ep=ep,
+              history_r=history_r,
+              history_Q=history_Q))
 }
 
 
 ### EXAMPLE (LIKELIHOOD IS NORMAL)
 # Define a likelihood being N((1,1), I)
-local_likelihood <- function(x){
-  return(dmvnorm(x, mu=c(1, 1), Sigma=diag(2)))
+# local_likelihood <- function(x){
+#   return(dmvnorm(x, mu=c(1, 1), Sigma=diag(2)))
+# }
+
+# Notice that can check if a directory exists using 
+# dir.exists(file.path(getwd(), "paramfiles"))
+# if this doesnt work, one can replace getwd() with the correct "main" path
+# which usually is "/home/mauro/Documents/University/MHAAR/IS_sub3"
+# could use something like 
+# unlist(mclapply(1:nsites, function(i) local_like(i, diag(3), c(0,0,0), 1)))
+local_likelihood <- function(site_index, sigma, mu, isss=1000){
+  # Construct file name containing chunk of data
+  target <- paste("genetree/gtree_file", site_index, sep="")
+  # Construct name of the file containing the sample from the cavity distribution
+  paramfile <- paste("paramfiles/paramfile_new", site_index, sep="")
+  # mu    : cavity distribution mean - prior mean for log(theta) and log(rho)
+  # sigma : cavity distribution vcov - prior vcov for log(theta) and log(rho)
+  nparams <- length(mu) # = 3 in this case with log(theta), log(R), log(tf), 
+  # simulate from widened cavity distribution (acts as a prior). Could use lgarch::rmnorm
+  trans.sigma <-  sigma
+  diag(trans.sigma) <-  2*diag(trans.sigma)
+  params = mnormt::rmnorm(n=1, mean = mu, varcov = trans.sigma) 
+  # use a widened distribution to improve stability
+  # correct it below in simwt
+  simwt = mnormt::dmnorm(params,mean=mu,varcov=sigma) / mnormt::dmnorm(params,mean=mu,varcov=trans.sigma)
+  write(t(params),paramfile,ncol=nparams)
+  use_ms_tf_scaling=T #see runnit.r for explanation
+  cm1 = paste("./is_moments_new",target,as.integer(1),as.integer(isss),paramfile,as.integer(use_ms_tf_scaling))
+  wvec = system(cm1,intern=T)
+  wvec = as.numeric(wvec)
+  wvec = simwt*wvec #apply correction
+  return(wvec)
 }
 
+
 # Settings
-nsites <- 20
-nparams <- 2
-nsamples <- 1000
-npasses <- 5
-mu_prior <- matrix(c(0,0))
-Sigma_prior <- diag(2)
-alpha <- 1.0
+nsites <- 5
+nsamples <- 500
+npasses <- 10
+mu_prior <- matrix(c(0,0,0))
+Sigma_prior <- diag(3)
+nparams <- nrow(Sigma_prior)
+# For Likelihood
+alpha <- 0.2
 
 # Run Algorithm
 a <- ep_algorithm(nsites, nparams, nsamples, npasses, 
                   mu_prior, Sigma_prior, local_likelihood, alpha)
 
 
+# After its done, to look at the history
+history_mu    <- array(0, dim=c(nparams, 1, npasses))
+history_Sigma <- array(0, dim=c(nparams, nparams, npasses))
+for (i in 1:npasses){
+  natural <- natural2moment(matrix(a$history_r[,,i]), a$history_Q[,,i])
+  history_mu[,,i] <- natural$mu
+  history_Sigma[,,i] <- natural$Sigma
+}
